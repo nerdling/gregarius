e4135804cde8b9b79e5e4c89530add6ca0351195
Jeremy Lavergne <github@lavergne.gotdns.org>
2011-12-02 21:00:01 -0500

    replace deprecated eregi

--- themes/default/mobile/nav.php
+++ themes/default/mobile/nav.php
@@ -5,7 +5,7 @@ $GLOBALS['rss']->nav->appendNavItem(getPath()."update.php?mobile", __('Refresh')
 $GLOBALS['rss']->nav->appendNavItem("#top",'TOP of Page');
 
 foreach ($GLOBALS['rss']->nav->items as $item) {
-    if( !eregi( 'update.php$', $item -> href ) && !eregi( 'admin', $item -> href ) ) {
+    if( !preg_match( '/update.php$/i', $item -> href ) && (stripos( $item->href, 'admin') === false) ) {
 			$item -> render();
     }
 }
